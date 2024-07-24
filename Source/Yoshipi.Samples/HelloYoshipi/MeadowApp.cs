fc655397b384df172aa88c8ba4a92afffab1e9cb
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-07-24 09:37:33 -0700

    Add HelloYoshipi

---
+++ Source/Yoshipi.Samples/HelloYoshipi/MeadowApp.cs
@@ -0,0 +1,24 @@
+﻿using Meadow;
+using System.Threading.Tasks;
+using YoshiPi;
+
+namespace HelloYoshipi;
+
+public class MeadowApp : YoshiPiApp
+{
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize...");
+
+        var displayController = new DisplayController(Hardware.Display!);
+
+        return Task.CompletedTask;
+    }
+
+    public override async Task Run()
+    {
+        Resolver.Log.Info("Run...");
+
+        await Task.CompletedTask;
+    }
+}

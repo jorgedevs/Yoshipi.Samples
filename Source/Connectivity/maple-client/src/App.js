cda8744b18f6cab30cc96807f469ad94ff68e672
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-09-05 11:36:28 -0700

    Revert "Replaced the Table Container"
    This reverts commit eaae95e5a022f61192b6c294504eddeba1b11475.

--- Source/Connectivity/maple-client/src/App.js
+++ Source/Connectivity/maple-client/src/App.js
@@ -1,42 +1,51 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App vh-100 d-flex">
-      <div className='p-5 bg-custom text-white'>
-          <div className='form-group d-inline align-items-center'>
-            <h1>Maple Client</h1>
-          </div>
-
-          <div className='form-group d-flex align-items-center p-2'>
-            <span className='mx-2'>IP Address</span>
-            <input className='form-control' style={{ width: '150px' }} />
-            <span className='mx-2'>Port</span>
-            <input className='form-control' style={{ width: '80px' }} />
-          </div>
-
-          <div className='form-group d-flex align-items-center p-2'>
-            <span className='mx-2'>Send text:</span>
-            <input className='form-control' style={{ width: '150px' }} />
-            <button className='btn btn-primary mx-2'>Send</button>
-          </div>
-
-          <div className='form-group d-flex align-items-center p-2'>
-            <span className='mx-2'>Sensor readings</span>
-            <button className='btn btn-primary mx-2'>Update</button>
-          </div>
-
-          <div className='form-group d-flex align-items-center p-2'>
-            <span className='mx-2'>Rotate servo</span>
-            <input type="range" min="0" max="180" value="90" className="form-range" id="myRange" />
-          </div>
-
-          <div className='form-group d-flex align-items-center p-2'>
-            <p className='font-weight-bold'>Built by @jorgedevs</p>
-          </div>
-
-      </div>
+      <Container className='p-5 bg-custom'>
+        <Row className="text-white text-center p-2">
+          <h1>Maple Client</h1>
+        </Row>
+        <Row>
+          <Col className="text-white text-center p-2">
+            <div className='form-group d-inline-flex align-items-center'>
+              <span className='mx-2'>IP Address</span>
+              <input className='form-control' style={{ width: '150px' }} />
+              <span className='mx-2'>Port</span>
+              <input className='form-control' style={{ width: '80px' }} />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-white text-center p-2">
+            <div className='form-group d-inline-flex align-items-center'>
+              <span className='mx-2'>Send text:</span>
+              <input className='form-control' style={{ width: '150px' }} />
+              <button className='btn btn-primary mx-2'>Send</button>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-white text-center p-2">
+            <div className='form-group d-inline-flex align-items-center'>
+              <h4>Get sensor readings</h4>
+              <button className='btn btn-primary mx-2'>Update</button>
+            </div>
+          </Col>
+        </Row>        
+        <Row>
+          <Col>
+            <div className='form-group d-inline-flex p-2 align-items-center'>
+              <p className='font-weight-bold text-white'>Built by @jorgedevs</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
